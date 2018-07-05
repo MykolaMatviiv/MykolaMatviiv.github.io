2fe4dff7a7f3515cea9108e640b4ae52adb2190f
MykolaMatviiv <matviivmykolav@gmail.com>
2018-07-05 08:14:58 +0300

    del Academy 18 - HTML, CSS (Matviiv Mykola)

--- Academy 18 - HTML, CSS (Matviiv Mykola)/app/js/common.js
+++
@@ -1,8 +0,0 @@
-
-jQuery('.mobile-nav').on("click", function () {
-	jQuery('#menu').toggleClass('active');
-});
-jQuery('#menu').append('<span id="close-nav"></span>');
-jQuery('#close-nav').on("click", function () {
-	jQuery('#menu').removeClass('active');
-});
